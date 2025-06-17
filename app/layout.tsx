0a5f96d4ed4d846b5ff9225c29e43c5d6016bb98
Alex Poputa <poputa.alex@gmail.com>
2025-06-17 09:07:15 +0300

    forgot the font

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,16 @@ import type { Metadata } from "next";
 import "./globals.css";
 import QueryProvider from "@/lib/components/QueryProvider";
 import NavBar from "@/lib/components/NavBar";
+import localFont from "next/font/local";
 
 export const metadata: Metadata = {
   title: "Foodies",
   description: "Your AI powered food inspiration",
 };
+
+const outfit = localFont({
+  src: "../lib/assets/fonts/Outfit-VariableFont.woff2",
+});
 
 export default function RootLayout({
   children,
@@ -15,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased flex flex-col items-center py-16`}>
+      <body
+        className={`${outfit.className} antialiased flex flex-col items-center py-16`}
+      >
         <NavBar />
         <QueryProvider>{children}</QueryProvider>
       </body>
